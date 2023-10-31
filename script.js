34d8fca0b1a79a027e366176a8c68ecf57869e23
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-10-31 14:59:06 +0800

    Create function to create new book

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
 const myLibrary = [];
+const addBtn = document.querySelector('#addBtn');
+const closeBtn = document.querySelector('#closeBtn');
+const bookDialog = document.querySelector('#bookDialog');
+const newDiv = document.createElement('div');
+const formInputs = document.querySelectorAll('input');
+const form = document.querySelector('form');
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  // Book object constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
-  const book = prompt('What book do you want?');
+function addBookToLibrary(data) {
+  // Create book from form data
+  const newBook = new Book(
+    form.title.value,
+    form.author.value,
+    form.pages.value,
+    form.read.value
+  );
+  console.log(`New book created = ${newBook}`);
+  // Push book to myLibrary array
+  myLibrary.push(newBook);
 }
+
+function displayBook(book) {
+  // create a div for book and append all the details to it
+}
+
+addBtn.addEventListener('click', () => {
+  bookDialog.showModal();
+});
+
+addBookBtn.addEventListener('click', () => {
+  // get all data from form
+  addBookToLibrary(form);
+
+  // Close dialog & reset form inputs after book is submitted
+  bookDialog.close();
+  formInputs.forEach((input) => {
+    input.value = '';
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  bookDialog.close();
+  formInputs.forEach((input) => {
+    input.value = '';
+  });
+});
